6afae0f49d046e1472e0807957851f8fbe40cc1b
AlexDeFoc <alex.de.foc@gmail.com>
2026-06-11 13:15:29 +0300

    Implemented Arguments Parsing flags into app + added dependency to octokit (github lib)

---
+++ MainApp/BenchmarkPrograms.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace MainApp;
+
+public static class BenchmarkPrograms
+{
+    public static void AppStartupTime(Action appInit)
+    {
+        var clock = Stopwatch.StartNew();
+        appInit();
+        clock.Stop();
+
+        Console.WriteLine($"[Info]: Elapsed time: {clock.ElapsedMilliseconds:F3} ms");
+        Console.ReadKey();
+    }
+}
