fb51008a09c7857a0d3b9cbb7cfdf985f95f707f
AlexDeFoc <alex.de.foc@gmail.com>
2026-07-13 01:29:13 +0300

    - fixed warnings from prev commit (works only on windows [with clang-cl & msvc]) - fixed by adding suppressing headers + disabling compiler errors from clang-cl & msvc
    Commiting to continue on Linux

--- Code/Clients/Cli/Tests/Includes/pch.hpp
+++ Code/Clients/Cli/Tests/Includes/pch.hpp
@@ -1,31 +1,5 @@
-#ifndef _GW_CLIENT_CLI_TESTS_PCH_HPP_
-#define _GW_CLIENT_CLI_TESTS_PCH_HPP_
+#pragma once
 
-// 1. Save the current warning state
-#if defined(_MSC_VER)
-    #pragma warning(push, 0) // Push and set warning level to 0 (turns off all warnings)
-#elif defined(__clang__)
-    #pragma clang diagnostics push
-    #pragma clang diagnostics ignored "-Wall"
-    #pragma clang diagnostics ignored "-Wextra"
-#elif defined(__GNUC__)
-    #pragma GCC diagnostics push
-    #pragma GCC diagnostics ignored "-Wall"
-    #pragma GCC diagnostics ignored "-Wextra"
-#endif
-
-// ==========================================
-// --- NO WARNING ZONE ---
+#include "disable_warnings.hpp"
 #include <gtest/gtest.h>
-// ==========================================
-
-// 2. Restore the original warning state
-#if defined(_MSC_VER)
-    #pragma warning(pop)
-#elif defined(__clang__)
-    #pragma clang diagnostics pop
-#elif defined(__GNUC__)
-    #pragma GCC diagnostics pop
-#endif
-
-#endif
+#include "enable_warnings.hpp"
